73e27e99bfe6728f9e549ee40bbcb37185ad17fd
hyedi.3 <hyeri9918as@gmail.com>
2023-02-05 00:52:08 +0900

    230205-study

---
+++ Practice03/01. check_figure_game/01_randomNumber.py
@@ -0,0 +1,8 @@
+# 임의의 숫자 생성 코드 만들기 
+
+import random      # 랜덤 모듈 불러오기 
+
+# random_number 변수에 1~100 사이의 임의의 값을 정수형태로 바인딩
+random_number = random.randint(1, 100)    # (최소값, 최대값)
+
+print(random_number)
---
+++ Practice03/01. check_figure_game/03_game_except.py
@@ -0,0 +1,29 @@
+# 숫자 맞추는 게임 만들기(예외처리)
+
+import random
+
+# random_number 변수에 1~100 사이의 임의의 값을 정수형태로 바인딩
+random_number = random.randint(1, 100)
+
+# 게임 횟수를 세기 위한 변수를 생성하고 1 값을 대입(바인딩)
+game_count = 1
+
+while True:      # while의 조건이 True이기 때문에 break를 만나기 전까지 계속 반복됨.
+    try :
+        # 입력 값을 정수형으로 변환 후 my_number 변수에 저장
+        my_number = int(input("1~100 사이의 숫자를 입력하세요. :"))
+
+        if my_number > random_number:  # 입력값이 임의의 수보다 크다면
+            print("down")               # down을 출력
+
+        elif my_number < random_number:  # 입력값이 임의의 수보다 작다면
+            print("up")                   # up을 출력
+
+        elif my_number == random_number:  # 입력값과 임의의 수 값이 같다면 (일치한다면)
+            print(f"축하합니다. {game_count}회 만에 맞췄습니다.")   # 맞춘 횟수 출력 (f'string)
+            break    # while 문 종료
+
+        game_count = game_count + 1   # while 문 반복 수행 시 count 값 1씩 증가 50
+        
+    except :    # 에러 발생 시 print문 출력 
+        print("에러가 발생했습니다. 숫자를 입력하세요 ")
